ac7b317edbd4d4d97f5bf3febd03f51a68668ea3
Poorva5 <poorvak455@gmail.com>
2024-06-27 21:21:11 +0530

    Ngnix setup

--- ams/settings/local.py
+++ ams/settings/local.py
@@ -14,6 +14,8 @@ SITE_NAME = getenv('SITE_NAME')
 
 SECRET_KEY = getenv("DJANGO_SECRET_KEY", 'DhFcyfg4CMJ7u1dSygWmrjousCriJ9fxuVDk67-Mh_DV8DFoU04')
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 ADMIN_URL = getenv('DJANGO_ADMIN_URL')
